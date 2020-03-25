3bbe80248ca26cafc76075cbae1e5a60b68fe63d
Peteradeleke <61970233+Peteradeleke@users.noreply.github.com>
2020-03-25 02:32:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var myName = "Adeleke Peter Ayooluwa";
+var myCourses = ["HTML", "CSS", "JavaScript"];
+console.log(myName);
+console.log(myCourses);
+
+for (let a = 1; a <= 200; a++) {
+	if (myCourses.length % 2 !== 0 & a % 2 !== 0) console.log(a);
+} 
